a13d76c7ed269a185a47c2437d49e1ab35c6afeb
artizanw <artizanw1@gmail.com>
2026-07-08 23:07:29 -0400

    afe init

--- lib/drivers/tle9012dqu/tle9012dqu.c
+++ lib/drivers/tle9012dqu/tle9012dqu.c
@@ -1,10 +1,9 @@
+#include <assert.h>
 #include <stdint.h>
 #include <stddef.h>
 #include "tle9012dqu.h"
+#include "../usart/usart.h"
 
-#define POLYNOMIAL 0x1D
-#define INITIAL_CRC 0xFF
-#define XOROUT 0xFF
 
 uint8_t crc8_j1850(const uint8_t* data, size_t len) {
 	uint8_t crc = INITIAL_CRC;
@@ -19,4 +18,22 @@ uint8_t crc8_j1850(const uint8_t* data, size_t len) {
 		}
 	}
 	return crc ^ XOROUT;
+}
+
+void TLE9012DQU_Init(uint8_t node_id) {
+	// Write ID
+	uint8_t write_frames[WRITE_FRAMES_LEN] = {SYNC_FRAME, WRITE_COMMAND, CONFIG_ADDR, 0x00, node_id}; // i really hope it's msb first
+	uint8_t write_crc = crc8_j1850(write_frames, WRITE_FRAMES_LEN);
+	USART3_Write(write_frames, WRITE_FRAMES_LEN, false);
+	USART3_Write(&write_crc, 1, false);
+	uint8_t reply_frame_buf[1] = {};
+	USART3_Read(reply_frame_buf, 1);
+	// Read back ID
+	uint8_t read_frames[READ_FRAMES_LEN] = {SYNC_FRAME, READ_COMMAND & node_id, CONFIG_ADDR};
+	uint8_t read_crc = crc8_j1850(read_frames, READ_FRAMES_LEN);
+	USART3_Write(read_frames, READ_FRAMES_LEN, false);
+	USART3_Write(&read_crc, 1, false);
+	uint8_t read_buf[READ_BYTES] = {};
+	USART3_Read(read_buf, READ_BYTES);
+	assert(((uint16_t)read_buf[1] << 8 | read_buf[2]) == node_id);
 }
--- lib/drivers/tle9012dqu/tle9012dqu.h
+++ lib/drivers/tle9012dqu/tle9012dqu.h
@@ -1,4 +1,17 @@
 #include <stdint.h>
 #include <stddef.h>
 
+#define POLYNOMIAL 0x1D
+#define INITIAL_CRC 0xFF
+#define XOROUT 0xFF
+
+#define WRITE_FRAMES_LEN 5
+#define READ_FRAMES_LEN 3
+#define READ_BYTES 4
+
+#define SYNC_FRAME 0x1E
+#define WRITE_COMMAND 0x80
+#define READ_COMMAND 0x7F
+#define CONFIG_ADDR 0x36
+
 uint8_t crc8_j1850(const uint8_t* data, size_t len);
