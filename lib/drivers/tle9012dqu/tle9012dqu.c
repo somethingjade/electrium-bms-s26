ffa81283c0012f904f032f168db231869021c840
artizanw <artizanw1@gmail.com>
2026-07-12 02:15:17 -0400

    add driver functions

--- lib/drivers/tle9012dqu/tle9012dqu.c
+++ lib/drivers/tle9012dqu/tle9012dqu.c
@@ -4,8 +4,7 @@
 #include "tle9012dqu.h"
 #include "../usart/usart.h"
 
-
-uint8_t crc8_j1850(const uint8_t* data, size_t len) {
+uint8_t CRC8_J1850(const uint8_t* data, size_t len) {
 	uint8_t crc = INITIAL_CRC;
 	for (size_t i = 0; i < len; ++i) {
 		crc ^= data[i];
@@ -20,20 +19,56 @@ uint8_t crc8_j1850(const uint8_t* data, size_t len) {
 	return crc ^ XOROUT;
 }
 
-void TLE9012DQU_Init(uint8_t node_id) {
+void TLE9012DQU_Send(const uint8_t* buf, size_t len) {
+	uint8_t crc = CRC8_J1850(buf, len);
+	USART3_Write(buf, len, false);
+	USART3_Write(&crc, 1, false);
+}
+
+void TLE9012DQU_HandleReplyFrame() {
+	uint8_t buf[1] = {};
+	USART3_Read(buf, 1); // TODO: handle the reply frame
+}
+
+uint16_t TLE9012DQU_ReadDataFrames() {
+	uint8_t buf[READ_BYTES] = {}; // idk if this even works
+				      // not sure how many bytes are read back
+				      // (crc, status, etc.)
+				      // for now im assuming its only the 2 data frames
+	USART3_Read(buf, READ_BYTES);
+	return ((uint16_t)(buf[0]) << 8) | buf[1];
+}
+
+void TLE9012DQU_Write(uint8_t id, uint8_t addr, uint16_t data) {
+	uint8_t frames[WRITE_FRAMES_LEN] = {SYNC_FRAME, WRITE_COMMAND | (NODE_ID_MASK & id), addr, (uint8_t)(data >> 8), (uint8_t)data};
+	TLE9012DQU_Send(frames, WRITE_FRAMES_LEN);
+	TLE9012DQU_HandleReplyFrame();
+}
+
+uint16_t TLE9012DQU_Read(uint8_t id, uint8_t addr) {
+	uint8_t frames[READ_FRAMES_LEN] = {SYNC_FRAME, READ_COMMAND | (NODE_ID_MASK & id), addr};
+	TLE9012DQU_Send(frames, READ_FRAMES_LEN);
+	return TLE9012DQU_ReadDataFrames();
+}
+
+void TLE9012DQU_Init(uint8_t nodeId) {
 	// Write ID
-	uint8_t write_frames[WRITE_FRAMES_LEN] = {SYNC_FRAME, WRITE_COMMAND, CONFIG_ADDR, 0x00, node_id}; // i really hope it's msb first
-	uint8_t write_crc = crc8_j1850(write_frames, WRITE_FRAMES_LEN);
-	USART3_Write(write_frames, WRITE_FRAMES_LEN, false);
-	USART3_Write(&write_crc, 1, false);
-	uint8_t reply_frame_buf[1] = {};
-	USART3_Read(reply_frame_buf, 1);
+	TLE9012DQU_Write(0x00, CONFIG_ADDR, nodeId);
 	// Read back ID
-	uint8_t read_frames[READ_FRAMES_LEN] = {SYNC_FRAME, READ_COMMAND & node_id, CONFIG_ADDR};
-	uint8_t read_crc = crc8_j1850(read_frames, READ_FRAMES_LEN);
-	USART3_Write(read_frames, READ_FRAMES_LEN, false);
-	USART3_Write(&read_crc, 1, false);
-	uint8_t read_buf[READ_BYTES] = {};
-	USART3_Read(read_buf, READ_BYTES);
-	assert(((uint16_t)read_buf[1] << 8 | read_buf[2]) == node_id);
+	uint16_t readId = TLE9012DQU_Read(nodeId, CONFIG_ADDR);
+	assert(readId == nodeId);
+}
+
+void TLE9012DQU_ReadPCVM(uint8_t nodeId, uint16_t* buf) {
+	TLE9012DQU_Write(nodeId, MEAS_CTRL_ADDR, (0b110 << CVM_MODE_POS) | PCVM_START);
+	while (TLE9012DQU_Read(nodeId, MEAS_CTRL_ADDR) & PCVM_START) {} // i feel like this isnt how i should be waiting for the measurement to complete
+	for (uint8_t i = 0; i < CELLS; ++i) {
+		buf[i] = TLE9012DQU_Read(nodeId, PCVM_BASE_ADDR + i);
+	}
+}
+
+uint16_t TLE9012DQU_ReadPCVMi(uint8_t nodeId, uint8_t i) {
+	TLE9012DQU_Write(nodeId, MEAS_CTRL_ADDR, (0b110 << CVM_MODE_POS) | PCVM_START);
+	while (TLE9012DQU_Read(nodeId, MEAS_CTRL_ADDR) & PCVM_START) {} // i feel like this isnt how i should be waiting for the measurement to complete
+	return TLE9012DQU_Read(nodeId, PCVM_BASE_ADDR + i);
 }
--- lib/drivers/tle9012dqu/tle9012dqu.h
+++ lib/drivers/tle9012dqu/tle9012dqu.h
@@ -7,12 +7,42 @@
 
 #define WRITE_FRAMES_LEN 5
 #define READ_FRAMES_LEN 3
-#define READ_BYTES 4
+#define READ_BYTES 2
+
+#define CELLS 12
 
 #define SYNC_FRAME 0x1E
+
 #define WRITE_COMMAND 0x80
-#define READ_COMMAND 0x7F
+#define READ_COMMAND 0x00
+#define NODE_ID_MASK 0x3F
+
+#define MEAS_CTRL_ADDR 0x18
+#define PCVM_BASE_ADDR 0x19
 #define CONFIG_ADDR 0x36
 
-uint8_t crc8_j1850(const uint8_t* data, size_t len);
-void TLE9012DQU_Init(uint8_t node_id);
+#define CVM_DEL_POS 0
+#define PBOFF_POS 5
+#define SCVM_START_POS 6
+#define AVM_START_POS 7
+#define BVM_MODE_POS 8
+#define BVM_START_POS 11
+#define CVM_MODE_POS 12
+#define PCVM_START_POS 15
+
+#define PBOFF (0b1 << PBOFF_POS)
+#define SCVM_START (0b1 << SCVM_START_POS)
+#define AVM_START (0b1 << AVM_START_POS)
+#define BVM_START (0b1 << BVM_START_POS)
+#define PCVM_START (0b1 << PCVM_START_POS)
+
+
+uint8_t CRC8_J1850(const uint8_t* data, size_t len); 
+void TLE9012DQU_Send(const uint8_t* buf, size_t len); 
+void TLE9012DQU_HandleReplyFrame(); 
+uint16_t TLE9012DQU_ReadDataFrames(); 
+void TLE9012DQU_Write(uint8_t id, uint8_t addr, uint16_t data); 
+uint16_t TLE9012DQU_Read(uint8_t id, uint8_t addr); 
+void TLE9012DQU_Init(uint8_t nodeId); 
+void TLE9012DQU_ReadPCVM(uint8_t nodeId, uint16_t* buf); 
+uint16_t TLE9012DQU_ReadPCVMi(uint8_t nodeId, uint8_t i); 
